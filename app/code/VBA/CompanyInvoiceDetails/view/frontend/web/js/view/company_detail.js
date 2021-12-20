8226e51cc081eac194a980464f1ee905efc4593a
kiennv <kiennguyenvan188@gmail.com>
2021-12-21 00:41:32 +0700

    Fix

--- app/code/VBA/CompanyInvoiceDetails/view/frontend/web/js/view/company_detail.js
+++ app/code/VBA/CompanyInvoiceDetails/view/frontend/web/js/view/company_detail.js
@@ -3,8 +3,9 @@ define([
     'ko',
     'uiComponent',
     'mage/url',
-    'Magento_Checkout/js/model/quote'
-], function ($, ko, Component, urlBuilder, quote) {
+    'Magento_Checkout/js/model/quote',
+    'Magento_Checkout/js/view/billing-address'
+], function ($, ko, Component, urlBuilder, quote, billing) {
     'use strict';
     return Component.extend({
         defaults: {
@@ -12,14 +13,36 @@ define([
             showError: ko.observable(false),
             showForm: ko.observable(true)
         },
+
         initialize: function () {
-            this._super();
+            this._super().observe({
+                isAddressSameAsShipping: true,
+            });
+
+
             // this.company = ko.observable(checkoutConfig.quoteData.extension_attributes.company);
             this.companyLegalName = ko.observable(checkoutConfig.quoteData.extension_attributes.company_legal_name);
             this.companyAddress = ko.observable(checkoutConfig.quoteData.extension_attributes.company_address);
             this.vatTax = ko.observable(checkoutConfig.quoteData.extension_attributes.vat_tax);
             this.companyRepresentative = ko.observable(checkoutConfig.quoteData.extension_attributes.company_representative);
             this.companyEmail = ko.observable(checkoutConfig.quoteData.extension_attributes.company_email);
+            this.recheckAddressSameAsShipping();
+        },
+
+        recheckAddressSameAsShipping : function (e) {
+            $(document).ready(function (){
+                var check = 0
+
+                $(document).on('change', 'input[name="billing-address-same-as-shipping"]', function () {
+                    var fieldSet = $('.fieldset.company-invoice-detail-fieldset');
+
+                    if(this.checked) {
+                        fieldSet.show();
+                    }else {
+                        fieldSet.hide();
+                    }
+                })
+            });
         },
 
         // setCompany: function (e) {
@@ -30,18 +53,22 @@ define([
             var self = this;
             checkoutConfig.quoteData.extension_attributes.company_legal_name = $('#company_legal_name').val();
         },
+
         setCompanyAddress: function (e) {
             var self = this;
             checkoutConfig.quoteData.extension_attributes.company_address = $('#company_address').val();
         },
+
         setVatTax: function (e) {
             var self = this;
             checkoutConfig.quoteData.extension_attributes.vat_tax = $('#vat_tax').val();
         },
+
         setCompanyRepresentative: function (e) {
             var self = this;
             checkoutConfig.quoteData.extension_attributes.company_representative = $('#company_representative').val();
         },
+
         setCompanyEmail: function (e) {
             var self = this;
             checkoutConfig.quoteData.extension_attributes.company_email = $('#company_email').val();
